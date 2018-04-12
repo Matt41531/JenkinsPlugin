d83b2b0e4c9ca634924c2219e9918330bd53e97d
Matthew Rife <dxfan218@yahoo.com>
2018-04-12 13:32:08 -0400

    Added maths.java file
    Created maths.java file for all the calculations the calculator performs

---
+++ maths.java
@@ -0,0 +1,49 @@
+package test;
+
+public class maths {
+
+    double add(double a, double b) {
+        double answer = a+b;
+        return answer;          
+    }
+    int add(int a, int b) {
+    	int answer = a+b;
+    	return answer;
+    }
+
+    double subtract(double a, double b) {
+        double answer = a-b;
+        return answer;          
+    }
+
+    int subtract(int a, int b) {
+    	int answer = a-b;
+    	return answer;
+    }
+    
+    double multiply(double a, double b) {
+        double answer = a*b;
+        return answer;          
+    }
+    
+    int multiply(int a, int b) {
+    	int answer = a*b;
+    	return answer;
+    }
+
+    double divide(double a, double b) {
+        double answer = a/b;
+        return answer;          
+    }
+
+    double power(double a, double b){
+        double answer =a;
+
+        for (int x=2; x<=b; x++){
+            answer *= a;
+        }
+
+        return answer;
+    }
+
+}
