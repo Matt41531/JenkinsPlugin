026da5bd1deb57a3ab73ba6cf593242c479e9822
Matthew Rife <mari242@uky.edu>
2018-04-13 10:14:50 -0400

    Updated HelloWorldBuilder to incorportate the code from calculator to get the plugin to take expressions from user and return the edge cases

--- HelloWorldBuilder.java
+++ HelloWorldBuilder.java
@@ -1,5 +1,7 @@
 package io.jenkins.plugins.sample;
 
+import java.util.Scanner;
+
 import hudson.Launcher;
 import hudson.Extension;
 import hudson.FilePath;
@@ -35,6 +37,92 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
     public boolean isUseFrench() {
         return useFrench;
     }
+    
+    public static String calculate(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        maths Maths = new maths();
+
+        double zeroAnswer = 0; //Possible refactor needed here at some point
+        double negAnswer = 0; //Change variable names
+        double posAnswer = 0;
+        int randAnswer = 0;
+        boolean positive = false;
+        double inputA, inputB, inputVar, inputEq;
+        char inputStr;
+        char operator;
+        boolean done = false;
+
+         while (done == false) {
+        	positive = false;
+            System.out.print("Please enter your expression: ");
+            randAnswer = ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+            inputA = input.next().charAt(0);
+            inputA = 0;
+            operator = input.next().charAt(0);
+            inputB = input.nextDouble();        
+            switch (operator) {
+                case '+': {
+                			zeroAnswer = Maths.subtract(inputA, inputB);
+                			negAnswer = zeroAnswer + 1; 
+                }
+                          break;
+                case '-': {
+                			zeroAnswer = Maths.add(inputA, inputB);
+                			negAnswer = zeroAnswer - 1;
+                }
+                          break;
+                case '*': {
+                			zeroAnswer = Maths.divide(inputA, inputB);
+                			negAnswer = -1;
+                }
+                          break;
+                case '/': {
+                			zeroAnswer = Maths.multiply(inputA, inputB);
+                			negAnswer = -1;
+                }
+                          break;
+                case '^': {
+                			zeroAnswer = Maths.power(inputA, inputB);
+                			negAnswer = 1;
+                }
+                		  break;
+                case '>': {
+                			zeroAnswer = inputB;
+                			negAnswer = inputB - 1;
+                			posAnswer = inputB + 1;
+                			positive = true;
+                }
+                		  break;
+                case '<': {
+                			zeroAnswer = inputB;
+                			negAnswer = inputB + 1;
+                			posAnswer = inputB - 1;
+                			positive = true;
+                }
+                		  break;
+                
+            }
+            
+            	System.out.print("Possible edge cases for " + inputStr + " are: ");
+            	if(!positive)
+            	{
+            		String result = (Double.toString(zeroAnswer) + ',' + Double.toString(negAnswer) + ',' + Integer.toString(randAnswer));   
+            	}
+            	else 
+            	{
+            		string result = (Double.toString(zeroAnswer) + ',' + Double.toString(negAnswer) + ',' + Double.toString(posAnswer) + ',' + Double.toString(randAnswer));
+            	}
+        }       
+
+        input.close();
+        return result;
+
+      }
+
+    }
 
     @DataBoundSetter
     public void setUseFrench(boolean useFrench) {
@@ -46,7 +134,7 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
         if (useFrench) {
             listener.getLogger().println("Bonjour, " + name + "!");
         } else {
-            listener.getLogger().println("Hello, " + name + "!");
+            listener.getLogger().println(calculate(name));
         }
     }
 
